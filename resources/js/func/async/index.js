edbddabecd2d96b4ef9724f0c1776c54aedc7348
myckhel <myckhel1@hotmail.com>
2021-06-10 23:47:38 +0100

    added useReduxRequest hook

--- resources/js/func/async/index.js
+++ resources/js/func/async/index.js
@@ -1,35 +1,139 @@
 import Http from "../Http";
-import { useEffect } from "react";
+import { useEffect, useCallback, useLayoutEffect } from "react";
 import useState from "use-react-state";
+import { EventRegister } from "react-native-event-listeners";
+import { useReduxState } from "use-redux-states";
 
-// import {readErrors} from "../";
+export const useEventListener = ({
+  names = [],
+  params,
+  callback,
+  removeCallback,
+}) => {
+  useLayoutEffect(() => {
+    if (names?.length && names.map) {
+      const listeners = names.map((name, i) =>
+        EventRegister.on(
+          name,
+          (payload) => callback && callback(name, payload, params)
+        )
+      );
 
-// const store = window.store
-// const Notify = window.Notify
+      return () =>
+        listeners.map((listener, i) => {
+          EventRegister.rm(listener);
+          removeCallback && removeCallback(names[i], params);
+        });
+    }
+  }, names);
 
-export const useRequest = ({ request, onSuccess, params }, dep = []) => {
-  const [state, setState] = useState({
-    data: undefined,
-    error: null,
-    status: null,
-    loading: false,
+  const emit = (...props) => EventRegister.emit(...props);
+
+  return { emit };
+};
+
+const useBaseRequest = ({
+  setState,
+  asyncRequest,
+  params = [],
+  state,
+  dep = [],
+  loadOnMount = true,
+  emits = [],
+  eventNames = [],
+  eventCallback,
+  eventRemoveCallback,
+  eventParams,
+  onSuccess,
+  setData = (data) => data,
+}) => {
+  const { emit } = useEventListener({
+    names: eventNames,
+    params: eventParams,
+    callback: eventCallback,
+    removeCallback: eventRemoveCallback,
   });
-  useEffect(() => {
-    makeRequest(params);
+
+  const request = useCallback(
+    async (...p) => {
+      try {
+        setState({ loading: true });
+        const { data, status } = await asyncRequest(...p);
+
+        onSuccess && onSuccess(data);
+
+        emits.map((event) => emit(event, data));
+
+        setState({ loading: false, status, data: setData(data) });
+      } catch (e) {
+        setState({ error: e, loading: false });
+        console.log({ e });
+      }
+    },
+    [asyncRequest]
+  );
+
+  useLayoutEffect(() => {
+    if (loadOnMount) {
+      request(...params);
+    }
   }, dep);
 
-  const makeRequest = async (params) => {
-    try {
-      setState({ loading: true });
-      const { data, status } = await request(params);
-      onSuccess && onSuccess(data);
-      setState({ data, status, loading: false });
-    } catch (e) {
-      setState({ error: e, loading: false });
-    }
-  };
+  return { ...state, setState, request };
+};
 
-  return { setState, request: makeRequest, ...state };
+const defaultRequestState = {
+  timestamp: new Date().getTime(),
+  error: null,
+  error: null,
+  status: null,
+  loading: false,
+};
+
+export const useRequest = ({ request, params, ...props }, dep = []) => {
+  const [state, setState] = useState(defaultRequestState);
+
+  return useBaseRequest({
+    setState,
+    asyncRequest: request,
+    params: [params],
+    state,
+    dep,
+    ...props,
+  });
+};
+
+const stateSelector = (s) => s || defaultRequestState;
+
+export const useReduxRequest = (
+  {
+    name,
+    resolver,
+    request: asyncRequest,
+    params,
+    state: reduxState = stateSelector,
+    reducer,
+    ...props
+  },
+  dep
+) => {
+  const { setState, useMemoSelector, selector } = useReduxState({
+    name,
+    state: reduxState,
+    reducer,
+  });
+
+  const state = useMemoSelector(selector, resolver);
+  const rest = useBaseRequest({
+    setState,
+    asyncRequest,
+    params: [params],
+    state,
+    dep,
+    ...props,
+  });
+
+  return { ...rest, selector };
 };
 
 const request = async (route, data = {}, method = "get", config = {}) => {
