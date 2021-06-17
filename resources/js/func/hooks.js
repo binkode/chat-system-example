331112891470b0442b1ec30c62ede953e709186f
myckhel <myckhel1@hotmail.com>
2021-06-17 01:41:34 +0100

    created `useRootMemoSelector` hook

--- resources/js/func/hooks.js
+++ resources/js/func/hooks.js
@@ -1,7 +1,29 @@
 import { usePage } from '@inertiajs/inertia-react'
+import { useSelector } from 'react-redux'
+import { get } from 'lodash'
+import { createSelector } from 'reselect'
+import isEqual from 'react-fast-compare'
 
 export const useProps = () => usePage().props
 
 export const useRoute = () => {
   return { params: new URLSearchParams(window.location.search) }
 }
+
+export const useRootMemoSelector = (
+  selectorOrName,
+  select = sel,
+  eq = isEqual
+) =>
+  useSelector(
+    createSelector(
+      isString(selectorOrName)
+        ? (state) => get(state, selectorOrName)
+        : selectorOrName,
+      select
+    ),
+    eq
+  )
+
+const sel = (state) => state
+const isString = (val) => typeof val === 'string'
