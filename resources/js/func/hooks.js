87640980246010ce5e8fd8d01e8422a032e87e97
myckhel <myckhel1@hotmail.com>
2021-11-06 00:59:15 +0100

    renamed `delivery` to `delivered`

--- resources/js/func/hooks.js
+++ resources/js/func/hooks.js
@@ -1,17 +1,17 @@
-import { useMemo } from 'react'
-import { usePage } from '@inertiajs/inertia-react'
-import { useSelector } from 'react-redux'
-import { get } from 'lodash'
-import { createSelector } from 'reselect'
-import isEqual from 'react-fast-compare'
-import moment from 'moment'
+import { useMemo } from "react";
+import { usePage } from "@inertiajs/inertia-react";
+import { useSelector } from "react-redux";
+import { get } from "lodash";
+import { createSelector } from "reselect";
+import isEqual from "react-fast-compare";
+import moment from "moment";
 
-export const useProps = () => usePage().props
+export const useProps = () => usePage().props;
 
 export const useRoute = () => {
-  const { url } = usePage()
-  return { url, params: new URLSearchParams(window.location.search) }
-}
+  const { url } = usePage();
+  return { url, params: new URLSearchParams(window.location.search) };
+};
 
 export const useRootMemoSelector = (
   selectorOrName,
@@ -26,37 +26,37 @@ export const useRootMemoSelector = (
       select
     ),
     eq
-  )
+  );
 
-const sel = (state) => state
-const isString = (val) => typeof val === 'string'
+const sel = (state) => state;
+const isString = (val) => typeof val === "string";
 
 export const useConversationEventType = (id) => {
   const chat_events = useRootMemoSelector(
     `msg.conversations.${id}`,
-    ({ read, delivery, created_at } = {}) => ({ read, delivery, created_at })
-  )
+    ({ read, delivered, created_at } = {}) => ({ read, delivered, created_at })
+  );
 
-  return useEventType(chat_events)
-}
+  return useEventType(chat_events);
+};
 
-export const useEventType = ({ read, delivery, created_at }) =>
+export const useEventType = ({ read, delivered, created_at }) =>
   useMemo(() => {
     if (read) {
       if (
         !created_at ||
         (created_at && moment(read.created_at).isAfter(created_at))
       ) {
-        return 'read'
+        return "read";
       }
     }
 
-    if (delivery) {
+    if (delivered) {
       if (
         !created_at ||
-        (created_at && moment(delivery.created_at).isAfter(created_at))
+        (created_at && moment(delivered.created_at).isAfter(created_at))
       ) {
-        return 'deliver'
+        return "deliver";
       }
     }
-  }, [read, delivery, created_at])
+  }, [read, delivered, created_at]);
