dbb7e6b4762250526e109c785ce7c10b5e6e8ca4
Myckhel <myckhel1@hotmail.com>
2022-12-13 14:35:47 +0100

    bumped tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,31 @@
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("cssnano")({
-      preset: "default",
-    }),
+const windmill = require("windmill-react-ui-kit/config");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const tailwindcss = windmill({
+  purge: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        bottom:
+          "0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)",
+      },
+    },
+  },
+  plugins: [],
+});
+
+module.exports = {
+  plugins: {
+    tailwindcss,
+    autoprefixer: {},
+    cssnano: {
+      preset: "default",
+    },
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,11 @@ const windmill = require("@windmill/react-ui/config");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = windmill({
-  purge: [
+  content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.jsx",
   ],
-  mode: "jit",
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
@@ -19,9 +17,6 @@ module.exports = windmill({
           "0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 });
